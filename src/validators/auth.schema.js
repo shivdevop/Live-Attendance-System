de4771b7880f01408cb132431a53d69bc3f00737
shivam <shivamagarwalla05@gmail.com>
2025-12-19 16:34:49 +0530

    defined validations for request schemas-signup&login schema using ZOD

---
+++ src/validators/auth.schema.js
@@ -0,0 +1,15 @@
+import {z} from "zod"
+
+export const signupSchema=z.object({
+    name:z.string().min(2,"name is required"),
+    email:z.string().email("invalid email format"),
+    password:z.string().min(6).max(20,"password must be bw 6 and 20 chars"),
+    role:z.enum(["teacher","student"],"role is required")
+
+})
+
+export const loginSchema=z.object({
+    email: z.string().eamil(),
+    password:z.string.min(6).max(20)
+})
+
