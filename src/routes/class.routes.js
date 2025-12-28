818e0d9c55e2cd34831be0532a1ab5210ad86605
shivam <shivamagarwalla05@gmail.com>
2025-12-28 17:45:32 +0530

    added route for starting the attendance

--- src/routes/class.routes.js
+++ src/routes/class.routes.js
@@ -1,9 +1,10 @@
 import express from "express"
-import {createClass,addStudentToClass,getClassDetails} from "../controllers/class.controller.js"
+import {createClass,addStudentToClass,getClassDetails,startAttendance} from "../controllers/class.controller.js"
 import {validateInput} from "../middlewares/validate.middleware.js"
 import {classSchema,studentIdSchema} from "../validators/class.schema.js"
 import {authenticate} from "../middlewares/auth.middleware.js"
 import {requireTeacher} from "../middlewares/role.middleware.js"
+import {attendanceSchema} from "../validators/attendance.schema.js"
 
 
 const router=express.Router()
@@ -14,6 +15,8 @@ router.post("/:classId/add-student",authenticate,requireTeacher,validateInput(st
 //get a class details
 router.get("/:classId",authenticate,getClassDetails)
 
+//start attendance for a class (only teachers can start attendance)
+router.post("/start-attendance",authenticate,requireTeacher,validateInput(attendanceSchema),startAttendance)
 
 
 export default router
