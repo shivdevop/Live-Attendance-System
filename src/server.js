934016a5028edaccaa510b55f16b8f949137e119
shivam <shivamagarwalla05@gmail.com>
2025-12-28 18:54:19 +0530

    fix: made changes to server.js to initialize both http and socket.io servers

--- src/server.js
+++ src/server.js
@@ -1,13 +1,24 @@
 import app from "./app.js"
 import {connectDB} from "./config/db.js"
 import { ENV } from "./config/env.js"
+import {initSocket} from "./socket/index.js"
+import http from "http"
 
 
 const startServer=async()=>{
     try {
         await connectDB()
-        app.listen(ENV.PORT,()=>{
+
+        //create http server from express app 
+        const httpServer=http.createServer(app)
+        
+        //initialize socket.io server
+        initSocket(httpServer)
+        
+        //start http server
+        httpServer.listen(ENV.PORT,()=>{
             console.log(`server is running on port ${ENV.PORT}`)
+            console.log("socket server is running")
         })
 
     } catch (error) {
